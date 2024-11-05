a49e635d6fbb20d70f19b1b709ec5b02009a75f3
fjeagle <huanleying@sina.com>
2024-11-05 13:51:39 +0800

    fix [repo使用aliyun镜像]

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,18 @@
 pluginManagement {
     repositories {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/releases")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -14,6 +27,18 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/releases")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+        }
         google()
         mavenCentral()
     }
